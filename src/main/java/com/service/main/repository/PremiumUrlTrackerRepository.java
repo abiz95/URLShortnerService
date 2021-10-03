0062be0978022a9dff3672fd748b0e3951390ee6
abiz95 <abintitusjohn@gmail.com>
2021-10-03 13:20:43 +0530

    Update PremiumUrlTrackerRepository.java

--- src/main/java/com/service/main/repository/PremiumUrlTrackerRepository.java
+++ src/main/java/com/service/main/repository/PremiumUrlTrackerRepository.java
@@ -20,10 +20,10 @@ public interface PremiumUrlTrackerRepository extends JpaRepository<PremiumUrlTra
 	@Query("select sum(p.hits) from PremiumUrlTrackerEntity p where p.userId=:userId and year(p.hitDate)=:hitYear and month(p.hitDate)=:hitMonth") 
 	Integer getSumOfMonthlyHits(@Param("userId") String userId, @Param("hitMonth") Integer hitMonth, @Param("hitYear") Integer hitYear);
 	
-	@Query(value="SELECT t.premium_url_tracker_id_rec, t.cre_rec_ts, t.hit_date, t.hits, t.shorten_url, t.upd_rec_ts, t.user_id FROM url_schema.premium_url_tracker t WHERE EXTRACT(DAY FROM t.hit_date) = EXTRACT(DAY FROM NOW()) AND t.user_id=:userId ORDER BY hits DESC LIMIT 3", nativeQuery = true) 
+	@Query(value="SELECT t.premium_url_tracker_id_rec, t.cre_rec_ts, t.hit_date, t.hits, t.shorten_url, t.upd_rec_ts, t.user_id FROM premium_url_tracker t WHERE EXTRACT(DAY FROM t.hit_date) = EXTRACT(DAY FROM NOW()) AND t.user_id=:userId ORDER BY hits DESC LIMIT 3", nativeQuery = true) 
 	List<PremiumUrlTrackerEntity> getDailyHits(@Param("userId") String userId);
 	
-	@Query(value="SELECT t.premium_url_tracker_id_rec, t.cre_rec_ts, t.hit_date, t.hits, t.shorten_url, t.upd_rec_ts, t.user_id FROM url_schema.premium_url_tracker t WHERE EXTRACT(MONTH FROM t.hit_date) = EXTRACT(MONTH FROM NOW()) AND t.user_id=:userId ORDER BY hits DESC LIMIT 5", nativeQuery = true) 
+	@Query(value="SELECT t.premium_url_tracker_id_rec, t.cre_rec_ts, t.hit_date, t.hits, t.shorten_url, t.upd_rec_ts, t.user_id FROM premium_url_tracker t WHERE EXTRACT(MONTH FROM t.hit_date) = EXTRACT(MONTH FROM NOW()) AND t.user_id=:userId ORDER BY hits DESC LIMIT 5", nativeQuery = true) 
 	List<PremiumUrlTrackerEntity> getMonthlyTopHits(@Param("userId") String userId);
 	
 }
